1afc4597be60b782eb60181b642f0fcf98e6a026
Eric Woroshow <eric@ericw.ca>
2011-08-12 15:06:52 -0700

    First commit. Skeleton of a game.

---
+++ builder.js
@@ -0,0 +1,92 @@
+/**
+ * @fileoverview Builder game.
+ */
+var builder = (function() {
+
+  /**
+   * Builder game.
+   * @constructor
+   */
+  function Builder() {
+  }
+
+  /**
+   * Starts the game loop.
+   */
+  Builder.prototype.start = function() {
+    var game = this;
+    var canvas = $('#canvas');
+
+    var frameTimeInMillis = 1000 / 30;
+    var frameTimeInSeconds = frameTimeInMillis / 1000;
+    var context = canvas[0].getContext('2d');
+
+    // Work around http://bugs.jquery.com/ticket/9678
+    setInterval(function() { }, 24*60*60*1000);
+
+    setInterval(function() {
+      game.update(frameTimeInSeconds);
+      game.draw(context);
+    }, frameTimeInMillis);
+  };
+
+  Builder.WIDTH = 640;
+  Builder.HEIGHT = 480;
+
+  /**
+   * Updates the game for the current frame.
+   * @param dt {number} The elapsed time, in seconds, since the last update
+   */
+  Builder.prototype.update = function(dt) {
+  };
+
+  /**
+   * Draw the game for the current frame.
+   * @param ctx {object} The cavnas context to which to draw
+   */
+  Builder.prototype.draw = function(ctx) {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, Builder.WIDTH, Builder.HEIGHT);
+
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 5;
+    ctx.strokeRect(0, 0, Builder.WIDTH, Builder.HEIGHT);
+  };
+
+
+  /**
+   * @return {boolean} If the browser supports the functionality we need.
+   */
+  function isSupported_() {
+    var canvas = document.createElement('canvas');
+    var supportsCanvas = !!(canvas.getContext);
+    var supportsText = !!(supportsCanvas && typeof canvas.getContext('2d').fillText === 'function');
+    return supportsCanvas && supportsText;
+  };
+
+  /**
+   * Indicates that external parts of the page are ready for the game to start.
+   * @type {boolean}
+   */
+  var ready_ = false;
+
+  /**
+   * Starts the game loop.
+   */
+  function start_() {
+    if (!isSupported_()) {
+      $('#unsupported').show();
+      $('#force-play').click(function() {
+        $('#unsupported').fadeOut();
+      });
+    }
+    // Even if the game is not supported, try to start it regardless so that
+    // the unsupported banner has a background... maybe.
+    new Builder().start();
+  }
+
+  return {
+    ready: ready_,
+    start: start_
+  }
+})();
---
+++ physics.js
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview Tiny physics libary based on Box2D.
+ */
+var physics = (function() {
+
+})();
