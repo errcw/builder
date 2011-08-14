6de26170dc2657f7268f878236550ba74c2190c1
Eric Woroshow <eric@ericw.ca>
2011-08-14 14:43:51 -0700

    Added more math functions.

--- physics.js
+++ physics.js
@@ -10,7 +10,7 @@ var physics = (function() {
   function Vec2(x, y) {
     this.x = x;
     this.y = y;
-  };
+  }
 
   Vec2.prototype.add = function(other) {
     this.x += other.x;
@@ -25,6 +25,45 @@ var physics = (function() {
   Vec2.prototype.len = function() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   };
+
+
+  /**
+   * A two-by-two matrix.
+   * @constructor
+   */
+  function Mat2(e11, e12, e21, e22) {
+    this.e11 = e11;
+    this.e12 = e12;
+    this.e21 = e21;
+    this.e22 = e22;
+  }
+
+  /**
+   * @param vec2 {Vec2} The vector to multiply
+   * @return {Vec2} The vector result
+   */
+  Mat2.prototype.mul = function(vec2) {
+    return new Vec2(
+        this.e11 * vec2.x + this.e12 * vec2.y,
+        this.e21 * vec2.x + this.e22 * vec2.y);
+  };
+
+  /**
+   * @return {Mat2} A copy of this matrix
+   */
+  Mat2.prototype.clone = function() {
+    return new Mat2(this.e11, this.e12, this.e21, this.e22);
+  };
+
+  /**
+   * @return {Mat2} A matrix representing the given angle in radians
+   */
+  Mat2.forRotation = function(angle) {
+    var c = Math.cos(angle);
+    var s = Math.sin(angle);
+    return new Mat22(c, -s, s, c);
+  };
+
 
   /**
    * A two-dimensional body.
@@ -46,6 +85,7 @@ var physics = (function() {
     this.surface_friction = 0.2;
   }
 
+
   /**
    * A rectangle.
    * @constructor
@@ -58,18 +98,18 @@ var physics = (function() {
   /**
    * @param position {Vec2} The position of the box
    * @param rotation {number} The rotation of the box, in radians
-   * @return {Array.<number>} The four corners of this box
+   * @return {Array.<Vec2>} The four corners of this box
    */
   Box.prototype.getPoints = function(position, rotation) {
-    //TODO
     var r = Mat2.forRotation(rotation);
     var hx = this.size.x * 0.5;
     var hy = this.size.y * 0.5;
-    return [ r.mul(Vec2(-h.x, -h.y)).add(position),
-             r.mul(Vec2(h.x, -h.y)).add(position),
-             r.mul(Vec2(h.x, h.y)).add(position),
-             r.mul(Vec2(-h.x, h.y)).add(position) ];
+    return [ r.clone().mul(new Vec2(-h.x, -h.y)).add(position),
+             r.clone().mul(new Vec2(h.x, -h.y)).add(position),
+             r.clone().mul(new Vec2(h.x, h.y)).add(position),
+             r.cloen().mul(new Vec2(-h.x, h.y)).add(position) ];
   };
+
 
   /**
    * A circle.
@@ -79,6 +119,7 @@ var physics = (function() {
     this.radius = radius;
     this.bounds = new BoundingBox(radius * 2, radius * 2);
   }
+
 
   /**
    * An axis-aligned bounding box for a shape.
